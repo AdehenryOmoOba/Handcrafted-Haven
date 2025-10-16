37fc1346fb497a6dc0d711cd311d839788be600b
olubisiayantoye <olubisiayantoye@gmail.com>
2025-10-16 13:55:31 +0100

    modified:   src/app/admin/page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,8 @@
 export default function Page() {
   return (
     <div>
-      <h1>Amin Dashboard</h1>
-      <p>This is a placeholder for thepage.</p>
+      <h1>Admin Dashboard</h1>
+      <p>This is a placeholder for the page.</p>
     </div>
   );
 }
-
